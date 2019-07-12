f9866cd3e5aea585c391d6f92dd7bd1f2f871106
mdaniel0828 <mdaniel0828@outlook.com>
2019-07-13 05:19:23 +0800

    home page watching area completed

--- src/components/Home/HomePage/index.tsx
+++ src/components/Home/HomePage/index.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react'
 import StayArea from '../StayArea'
 import CommunicationArea from '../CommunicationArea'
+import { ResponsiveImage, ResponsiveImageSize } from 'react-responsive-image'
+
 import './styles.scss'
+
+import home_watching_mobile from '../../../images/home_watching_mobile.png'
+import home_watching_landscape from '../../../images/home_watching_landscape.png'
 
 const HomePage: React.StatelessComponent<{}> = () => {
   return (
     <div className="HomePage">
       <StayArea />
       <CommunicationArea />
+      <div className="watching_area">
+        <div className="text">
+          <div className="heading">
+            Type like <span className="color_red">no</span> one’s watching...
+          </div>
+          <div className="content">
+            Because no one is. Communicate with the freedom you have when you
+            are talking face to face, safe in the knowledge that your
+            communications will remain confidential.
+          </div>
+        </div>
+        <ResponsiveImage className="watching_img">
+          <ResponsiveImageSize
+            default
+            minWidth={0}
+            path={home_watching_mobile}
+          />
+          <ResponsiveImageSize minWidth={576} path={home_watching_landscape} />
+        </ResponsiveImage>
+      </div>
     </div>
   )
 }
