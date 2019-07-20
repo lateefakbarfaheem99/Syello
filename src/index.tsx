cb7c28e11f7db614df5a93c9b715feaf5930efb8
mdaniel0828 <mdaniel0828@outlook.com>
2019-07-21 04:52:31 +0800

    loading O animation added.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { AppRouter } from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import WebFont from 'webfontloader'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppRouter } from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import WebFont from 'webfontloader';
 
 WebFont.load({
   google: {
     families: ['Raleway', 'sans-serif']
   }
-})
+});
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'))
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,60 @@
-import * as React from 'react'
-import { Route, HashRouter, Switch } from 'react-router-dom'
-import { Header, Footer, Contact } from './components'
-import TheDifferencePage from './components/TheDifferencePage'
-import HomePage from './components/Home/HomePage'
+import * as React from 'react';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+import { Header, Footer, Contact } from './components';
+import TheDifferencePage from './components/TheDifferencePage';
+import HomePage from './components/Home/HomePage';
 
-export const AppRouter: React.StatelessComponent<{}> = () => {
-  return (
-    <HashRouter>
-      <div className="">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/difference" component={TheDifferencePage} />
-          <Route path="/contact" component={Contact} />
-          <Route component={HomePage} />
-        </Switch>
-        <Footer />
-      </div>
-    </HashRouter>
-  )
+import Lottie from 'react-lottie';
+import animationData from './images/sylo-logo-animation.json';
+
+interface IState {
+  isLoading: boolean;
+}
+
+export class AppRouter extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+  // fake authentication Promise
+  private authenticate() {
+    return new Promise(resolve => setTimeout(resolve, 1500));
+  }
+
+  componentDidMount() {
+    this.authenticate().then(() => {
+      this.setState({ isLoading: false });
+      console.log(this.state.isLoading);
+    });
+  }
+
+  public render() {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return (
+      <HashRouter>
+        {this.state.isLoading ? (
+          <Lottie options={defaultOptions} height={400} width={400} />
+        ) : (
+          <div className="">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/difference" component={TheDifferencePage} />
+              <Route path="/contact" component={Contact} />
+              <Route component={HomePage} />
+            </Switch>
+            <Footer />
+          </div>
+        )}
+      </HashRouter>
+    );
+  }
 }
